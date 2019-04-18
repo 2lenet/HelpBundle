b86198d3542e9771c22bb969f754f801b1af27d0
joris <joris@2le.net>
2019-04-18 18:29:51 +0200

    src

--- Resources/public/js/help.js
+++
@@ -1,7 +0,0 @@
-window.lleShowHelpButton = function lleShowHelpButton() {
-    if ( $(".lleHelp-icon").css('display') == 'none') {
-        $(".lleHelp-icon").css("display","inline-block");
-    } else {
-        $(".lleHelp-icon").css("display","none");
-    }
-}
